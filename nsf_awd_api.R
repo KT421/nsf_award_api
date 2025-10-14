67f82803c1be82501a1a1c33086845db1b4d48e2
KT421 <krschreyer@gmail.com>
2025-10-14 12:01:20 -0700

    Add files via upload

---
+++ nsf_awd_api.R
@@ -0,0 +1,73 @@
+# Getting NSF award termination data from the public facing Award API
+
+library(tidyverse)
+
+#' Search NSF award api
+#' 
+#' This function is a wrapper for the public facing award API
+#' 
+#' NSF public facing award database API is described here: https://resources.research.gov/common/webapi/awardapisearch-v1.htm
+#'
+#' @param ... Desired search parameters as described in the API spec
+#' @param maxResults Number of results to return. API max is 10000 
+#' @param fields Fields to return, string separated by comma. Leave as NULL to get the default set marked in the API spec
+#'
+#' @returns dataframe of results from API
+#'
+#' @export
+#' @examples
+search_nsf_award_api <- function(..., maxResults = 500, fields = NA) {
+  
+  params <- paste(names(list(...)), list(...), collapse = "&", sep = "=")
+
+  printfields <- ifelse(!is.na(fields),paste0("&printFields=",fields),NA)
+
+  offset <- seq(from = 0, to = maxResults-1, by = 25)
+
+  urls <- paste0("https://www.research.gov/awardapi-service/v1/awards.json?",params,"&offset=",offset,printfields)
+
+  results <- NULL
+
+  for (i in 1:length(offset)) {
+    this_results <- jsonlite::fromJSON(URLencode(urls[i]))
+
+    this_results <- as.data.frame(this_results$response$award)
+
+    results <- dplyr::bind_rows(results,this_results)
+
+    if (nrow(this_results) < 25) break
+  }
+
+  results 
+}
+
+# Since awdSpAttnCode is not a searchable parameter, we have to get everything it could possibly be applied to
+# which means every award with an end date after terminations started occuring, the earlist of which is Apr 2025
+# Brute force; get every award with an end date in 2025 or later
+# chunked by end date because the API only returns 10000 results maximum 
+# MOST of the results will be in 2025 because award that are terminated have their end date changed to the termination date
+# but that change is not automatic and lags some weeks behind the awdSpAttnCode update
+results2025_1 <- search_nsf_award_api(expDateStart="01/01/2025",expDateEnd="06/30/2025", maxResults = 10000)
+results2025_2 <- search_nsf_award_api(expDateStart="07/01/2025",expDateEnd="12/31/2025", maxResults = 10000)
+results2026_1 <- search_nsf_award_api(expDateStart="01/01/2026",expDateEnd="06/30/2026", maxResults = 10000)
+results2026_2 <- search_nsf_award_api(expDateStart="07/01/2026",expDateEnd="08/31/2026", maxResults = 10000)
+results2026_3 <- search_nsf_award_api(expDateStart="09/01/2026",expDateEnd="12/31/2026", maxResults = 10000)
+results2027_1 <- search_nsf_award_api(expDateStart="01/01/2027",expDateEnd="06/30/2027", maxResults = 10000)
+results2027_2 <- search_nsf_award_api(expDateStart="07/01/2027",expDateEnd="12/31/2027", maxResults = 10000)
+results2028 <- search_nsf_award_api(expDateStart="01/01/2028",expDateEnd="12/31/2028", maxResults = 10000)
+results2029 <- search_nsf_award_api(expDateStart="01/01/2029",expDateEnd="12/31/2029", maxResults = 10000)
+results2030 <- search_nsf_award_api(expDateStart="01/01/2030", maxResults = 10000)
+
+
+all_results <- dplyr::bind_rows(results2025_1, results2025_2, results2026_1, results2026_2, results2026_3,
+                          results2027_1, results2027_2, results2028, results2029, results2030)
+
+
+all_results |> count(awdSpAttnCode)
+
+
+terminated_awds <- all_results |>
+  filter(awdSpAttnCode == '37') |>
+  mutate(expDate = mdy(expDate),
+        startDate = mdy(startDate)) |>
+  distinct() # not sure why there are some duplicates but there are
